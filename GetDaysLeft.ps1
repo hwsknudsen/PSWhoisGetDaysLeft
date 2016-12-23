51b6091db4f3c1498e22b28152daf91c3180c44c
hwsknudsen <hwsknudsen@users.noreply.github.com>
2016-12-22 22:20:36 -0400

    added custom dates + set whois location paramter

--- GetDaysLeft.ps1
+++ GetDaysLeft.ps1
@@ -1,17 +1,28 @@
+#whois.exe from micrsoft must be in specified in script or set as a avariable
 #example .\GetDaysLeft.ps1 rg.bm "Registrar Registration Expiration Date: " whois.asaplatform.info
 #example .\GetDaysLeft.ps1 google.com "Registrar Registration Expiration Date: "
 #example .\GetDaysLeft.ps1 bbc.co.uk "        Expiry date:" 
 
-#whois.exe from micrsoft must be in specified in script 
+#if whois server uses a custom date try something like below
+#.\GetDaysLeft.ps1 -domain fre.fr -expirationtextsearch "Expiry Date: " -dateformattstring "dd/MM/yyyy"
+#Custom Date and Time Format Strings https://msdn.microsoft.com/en-us/library/8kb3ddd4(v=vs.110).aspx
+
 Param(
   [Parameter(Mandatory=$true)]
   [string]$domain,
   [Parameter(Mandatory=$true)]
   [string]$expirationtextsearch,
-  [string]$whoisserveroveride
+  [string]$dateformattstring,
+  [string]$whoisserveroveride,
+  [string]$whoislocation
 )
 
-$Command = "C:\Program Files (x86)\PRTG Network Monitor\custom sensors\EXE\whois.exe"
+if(!$whoislocation){
+    $Command = "c:\Windows\System32\whois.exe"
+}else{
+    $Command = $whoislocation    
+}
+
 $Parms = "/accepteula $domain $whoisserveroveride"
 $Prms = $Parms.Split(" ")
 $whoisoutput = (& "$Command" $Prms) 
@@ -20,9 +31,13 @@ $expirationresult = $whoisoutput | % { if($_ -match $expirationtextsearch) {$_}}
 
 $datestring = $expirationresult.Substring($expirationtextsearch.Length)
 
-$expiredate = Get-Date $datestring
-$daystoexpire = ($expiredate - (Get-Date)).Days
+if (!$dateformattstring){
+    $expiredate = Get-Date $datestring
+}else{
+    $expiredate = [DateTime]::ParseExact($datestring, $dateformattstring, $null)
+}
 
+$daystoexpire = ($expiredate - (Get-Date)).Days
 
 $x=[string]$daystoexpire+":The domain registration expires in "+$daystoexpire+" days."
 write-host $x
